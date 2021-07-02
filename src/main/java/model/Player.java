e32d2006716057732cc7fab42d10b63bdb299c8f
ArianQazvini <a.qazvini@aut.ac.ir>
2021-07-02 10:20:08 +0430

    created a new branch

---
+++ src/main/java/model/Player.java
@@ -0,0 +1,11 @@
+package model;
+
+import java.util.ArrayList;
+
+public class Player {
+    private ArrayList<Card> Deck;
+    private String name;
+    private String password;
+    private int elxir =0;
+
+}
---
+++ src/main/java/model/Tower/PrinceTower.java
@@ -0,0 +1,7 @@
+package model.Tower;
+
+public class PrinceTower extends Tower{
+    public PrinceTower(int hp, int damage, int hitSpeed, int range) {
+        super(hp, damage, hitSpeed, range);
+    }
+}
