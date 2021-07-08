fbdf9039a98cf59d6777f6b5e081dbf7b7e36b99
ArianQazvini <a.qazvini@aut.ac.ir>
2021-07-08 19:01:46 +0430

    Game Manager Class

--- src/main/java/Controller/Controller.java
+++ src/main/java/Controller/Controller.java
@@ -6,10 +6,9 @@ import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.ListCell;
-import javafx.scene.control.ListView;
-import javafx.scene.control.TextField;
+import javafx.scene.Node;
+import javafx.scene.control.*;
+import javafx.scene.control.skin.CellSkinBase;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
@@ -18,10 +17,17 @@ import javafx.scene.layout.HBox;
 import javafx.scene.paint.ImagePattern;
 import javafx.util.Callback;
 import javafx.util.Duration;
+import model.GameManager;
 import sample.Fighter;
+import sample.PicHandler;
 
 import java.io.File;
+import java.security.KeyStore;
 import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Controller {
 
@@ -43,18 +49,28 @@ public class Controller {
     @FXML
     private Button Button4;
     @FXML
+    private ProgressBar ElxirBar;
+    @FXML
+    private TextField Elxirs;
+    @FXML
     private TextField Warnings;
     @FXML
     private AnchorPane PlayGround;
-    private Timeline timeline;
+    private Timer timer;
     private final int blockSize = 20;
-//    private final int rows =18;
-//    private final int cols = 32;
+    private final int rows =18;
+    private final int cols = 32;
     private ImageView[][] blocks = new ImageView[32][18];
     private ImageView[][] roads = new ImageView[32][2];
     private ImageView[][] river = new ImageView[2][16];
+    private GameManager gameManager;
+    private ArrayList<Fighter> elements = new ArrayList<>();
+    private int check =0;
+    //    private ArrayList<Fighter> fighters = new ArrayList<>();
     public void initialize()
     {
+        gameManager = new GameManager();
+        gameManager.CreateMap();
         Warnings.setVisible(false);
         Image image = new Image(new File("src/main/resources/pics/Wizard.jpg").toURI().toString());
         ImageView imageView = new ImageView(image);
@@ -63,72 +79,97 @@ public class Controller {
         imageView.setPreserveRatio(true);
         Button1.setGraphic(imageView);
         CreateMap();
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                StartTimer();
+            }
+        });
+        thread.setDaemon(true);
+        thread.start();
     }
-//    private void add()
-//    {
-//        Fighter fighter = new Fighter("Wizard",Speed.MID,"src/main/resources/pics/Wizard.jpg","src/main/resources/pics/Wizard.jpg");
-//        Image image = new Image(new File("src/main/resources/pics/Wizard.jpg").toURI().toString());
-//        ImageView imageView = new ImageView(image);
-//        imageView.setFitHeight(100);
-//        imageView.setFitWidth(100);
-//        Deck.getChildren().add(imageView);
-//    }
-
     @FXML
     void Press(ActionEvent event) {
-        if(event.getSource()==Button1)
-        {
-            PlayGround.setDisable(false);
-            PlayGround.setOnMouseClicked(new EventHandler<MouseEvent>() {
-                @Override
-                public void handle(MouseEvent mouseEvent) {
-                    if(mouseEvent.getY()<=340.0)
-                    {
-                        Warnings.setVisible(true);
-                        Warnings.setStyle("-fx-text-inner-color:red");
-                        Warnings.setText("NOT ALLOWED PLACE");
-                    }
-                    else
-                    {
-                        Warnings.setVisible(false);
-                        Thread thread = new Thread(new Runnable() {
-                            @Override
-                            public void run() {
-                                Task(mouseEvent.getX(),mouseEvent.getY());
-                            }
-                        });
-                        thread.setDaemon(true);
-                        thread.start();
-                    }
-                    PlayGround.setDisable(true);
+        PlayGround.setDisable(false);
+        PlayGround.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                if(mouseEvent.getY()<=340.0)
+                {
+                    Warnings.setVisible(true);
+                    Warnings.setStyle("-fx-text-inner-color:red");
+                    Warnings.setText("NOT ALLOWED PLACE");
                 }
-            });
-        }
-    }
+                else
+                {
+                    Warnings.setVisible(false);
 
-    private void Task(double x,double y)
+                    Thread thread = new Thread(new Runnable() {
+                        @Override
+                        public void run() {
+                            if(event.getSource()==Button1)
+                            {
+                                Task(mouseEvent.getX(),mouseEvent.getY(),300);
+                            }
+                            else
+                            {
+                                Task(mouseEvent.getX(),mouseEvent.getY(),400);
+
+                            }
+
+                        }
+                    });
+                    thread.setDaemon(true);
+                    thread.start();
+                }
+                PlayGround.setDisable(true);
+            }
+        });
+    }
+    private void ElxirStart()
+    {
+
+        Platform.runLater(new Runnable() {
+            Timeline timeline1 =null;
+            double i = 0.0;
+            int j =0;
+            @Override
+            public void run() {
+                KeyFrame keyFrame = new KeyFrame(Duration.seconds(2),actionEvent -> {
+                    Elxirs.setText(String.valueOf(j));
+                    ElxirBar.setProgress(i);
+                    i+=0.1;
+                    j++;
+                });
+                timeline1= new Timeline(keyFrame);
+                timeline1.setCycleCount(Timeline.INDEFINITE);
+                timeline1.play();
+            }
+
+        });
+    }
+    private void ElxirThread()
+    {
+        Thread thread= new Thread(new Runnable() {
+            @Override
+            public void run() {
+                ElxirStart();
+            }
+        });
+        thread.setDaemon(true);
+        thread.start();
+    }
+    private void Task(double x,double y,int seconds)
     {
         Platform.runLater(new Runnable() {
             @Override
             public void run() {
                 Fighter fighter = new Fighter("Wizard",Speed.MEDIUM,"src/main/resources/pics/Wizard.jpg","src/main/resources/pics/Wizard.jpg");
-                Image image = new Image(new File("src/main/resources/pics/Wizard.jpg").toURI().toString());
-                ImagePattern imagePattern = new ImagePattern(image);
-                fighter.setFill(imagePattern);
-                fighter.setHeight(20);
-                fighter.setWidth(20);
+                fighter.getPicHandler().setWalkingMode();
                 FixLocation(fighter,x,y);
-                PlayGround.getChildren().add(fighter);
-                KeyFrame keyFrame = new KeyFrame(Duration.seconds(0.3),event ->{
-                    if(fighter.getY()>0)
-                    {
-                        fighter.setY(fighter.getY()-blockSize);
-
-                    }
-                });
-                timeline = new Timeline(keyFrame);
-                timeline.setCycleCount(Timeline.INDEFINITE);
-                timeline.play();
+                fighter.setSeconds(seconds);
+                gameManager.getFighters().add(fighter);
+                PlayGround.getChildren().add(fighter.getPicHandler());
             }
         });
     }
@@ -144,8 +185,9 @@ public class Controller {
                 {
                     if(j==xPass)
                     {
-                        temp.setX(j*blockSize);
-                        temp.setY(i*blockSize);
+                        temp.setCurrent(j*blockSize,i*blockSize);
+//                          temp.setX(j*blockSize);
+//                          temp.setY(i*blockSize);
                         break;
                     }
                 }
@@ -154,8 +196,6 @@ public class Controller {
     }
     private void CreateMap()
     {
-        double help_col = 0.0;
-        double help_row = 0.0;
         for (int i=0;i<32;i++)
         {
             if(i==15 || i==16)
@@ -183,30 +223,14 @@ public class Controller {
                         {
                             index=1;
                         }
-                        Image image = new Image(new File("src/main/resources/pics/roadWater.png").toURI().toString());
-                        roads[i][index] = new ImageView(image);
-                        roads[i][index].setFitWidth(20);
-                        roads[i][index].setFitHeight(20);
-                        roads[i][index].setY(help_row);
-                        roads[i][index].setX(help_col);
-                        help_col += blockSize;
-                        PlayGround.getChildren().add(roads[i][index]);
+                        PlayGround.getChildren().add(gameManager.getRoads()[i][index]);
                     }
                     else
                     {
-                        Image image = new Image(new File("src/main/resources/pics/terrainTile6.png").toURI().toString());
-                        river[index2][z] = new ImageView(image);
-                        river[index2][z].setFitWidth(20);
-                        river[index2][z].setFitHeight(20);
-                        river[index2][z].setY(help_row);
-                        river[index2][z].setX(help_col);
-                        help_col += blockSize;
-                        PlayGround.getChildren().add(river[index2][z]);
+                        PlayGround.getChildren().add(gameManager.getRiver()[index2][z]);
                         z++;
                     }
                 }
-                help_col =0.0;
-                help_row+= blockSize;
             }
             else
             {
@@ -223,31 +247,71 @@ public class Controller {
                         {
                             index=1;
                         }
-                        Image image = new Image(new File("src/main/resources/pics/roadTile.png").toURI().toString());
-                        roads[i][index] = new ImageView(image);
-                        roads[i][index].setFitWidth(20);
-                        roads[i][index].setFitHeight(20);
-                        roads[i][index].setY(help_row);
-                        roads[i][index].setX(help_col);
-                        help_col += blockSize;
-                        PlayGround.getChildren().add(roads[i][index]);
+                        PlayGround.getChildren().add(gameManager.getRoads()[i][index]);
                     }
                     else
                     {
-                        Image image = new Image(new File("src/main/resources/pics/terrainTile3.png").toURI().toString());
-                        blocks[i][j] = new ImageView(image);
-                        blocks[i][j].setFitWidth(20);
-                        blocks[i][j].setFitHeight(20);
-                        blocks[i][j].setY(help_row);
-                        blocks[i][j].setX(help_col);
-                        help_col += blockSize;
-                        PlayGround.getChildren().add(blocks[i][j]);
+                        PlayGround.getChildren().add(gameManager.getBlocks()[i][j]);
                     }
                 }
-                help_col =0.0;
-                help_row+= blockSize;
             }
         }
     }
-}
+    private void StartTimer()
+    {
+        this.timer = new Timer();
+        TimerTask timerTask = new TimerTask() {
+            @Override
+            public void run() {
+                Platform.runLater(new Runnable() {
+                    @Override
+                    public void run() {
+                        update();
+                    }
+                });
+            }
+        };
+        this.timer.schedule(timerTask,0,200);
 
+    }
+    private void update()
+    {
+        if(gameManager.getFighters().size()!=0)
+        {
+//            for (int i=0;i<gameManager.getFighters().size();i++)
+//            {
+//               PlayGround.getChildren().add(gameManager.getFighters().get(i).getPicHandler());
+//            }
+
+            gameManager.Move();
+//            elements= gameManager.getFighters();
+//            remove();
+        }
+    }
+    private void remove()
+    {
+        Iterator<Node> it = PlayGround.getChildren().iterator();
+        int z = 0;
+        while (it.hasNext())
+        {
+            Node temp = it.next();
+            if(temp instanceof PicHandler)
+            {
+                PicHandler temp2 = (PicHandler) temp;
+                if(temp2.equals(elements.get(z).getPicHandler()))
+                {
+                    it.remove();
+                    z++;
+                }
+                else
+                {
+                }
+            }
+            else
+            {
+
+            }
+        }
+        elements.clear();;
+    }
+}
--- src/main/java/sample/Fighter.java
+++ src/main/java/sample/Fighter.java
@@ -1,29 +1,101 @@
 package sample;
 
 import enums.Speed;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.shape.Rectangle;
+import javafx.util.Duration;
 
-public class Fighter extends Rectangle {
+import java.io.File;
+import java.net.MalformedURLException;
+import java.nio.file.DirectoryStream;
+
+public class Fighter {
     private String name;
     private Speed speed;
     private String avatar;
     private String Walking;
+    private int x_destination;
+    private int y_destination;
+    private double x_Current;
+    private double y_Current;
+    private int seconds;
+    private PicHandler picHandler;
+    private Timeline timeline;
+    //  private GameManager gameManager;
     public Fighter(String name, Speed speed,String Avatar,String Walking) {
         this.name = name;
         this.speed = speed;
-        this.avatar=Avatar;
-        this.Walking=Walking;
+        this.avatar = Avatar;
+        this.Walking = Walking;
+        this.picHandler = new PicHandler(this,"src/main/resources/pics/Wizard.jpg");
     }
-
+    //    @Override
+//    public void run()
+//    {
+//
+////        KeyFrame keyFrame = new KeyFrame(Duration.seconds(seconds), event ->
+////        {
+////            if (y_Current > 0) {
+////                Forward();
+////            }
+////        });
+////        timeline=new Timeline(keyFrame);
+////        timeline.setCycleCount(Timeline.INDEFINITE);
+////        timeline.play();
+//        while(y_Current>0)
+//        {
+//            Directions help = GameManager.CharacterExist(x_Current,y_Current,Directions.TOP);
+//            if(help==null)
+//            {
+//                Forward();
+//            }
+//            else
+//            {
+//                Left();
+//                try {
+//                    Thread.sleep(seconds);
+//                } catch (InterruptedException e) {
+//                    e.printStackTrace();
+//                }
+//                Forward();
+//            }
+//            try {
+//                Thread.sleep(seconds);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        }
+//    }
+    public void Left()
+    {
+        x_Current = picHandler.getX()-20;
+        picHandler.setX(x_Current);
+    }
+    public void Right()
+    {
+        x_Current=picHandler.getX()+20;
+        picHandler.setX(x_Current);
+    }
+    public void Forward()
+    {
+        y_Current=picHandler.getY()-20;
+        picHandler.setY(y_Current);
+    }
+    public void Backward()
+    {
+        y_Current=picHandler.getY()+20;
+        picHandler.setY(y_Current);
+    }
     public void setWalking(String walking) {
         Walking = walking;
     }
-
     public String getWalking() {
         return Walking;
     }
 
-    public void setName(String name) {
+    public void SetName(String name) {
         this.name = name;
     }
 
@@ -31,7 +103,7 @@ public class Fighter extends Rectangle {
         this.speed = speed;
     }
 
-    public String getName() {
+    public String GetName() {
         return name;
     }
 
@@ -42,7 +114,6 @@ public class Fighter extends Rectangle {
     public void setAvatar(String avatar) {
         this.avatar = avatar;
     }
-
     public String getAvatar() {
         return avatar;
     }
@@ -51,4 +122,40 @@ public class Fighter extends Rectangle {
     {
         return this.avatar;
     }
+    public void setDestination(int x,int y)
+    {
+        this.x_destination=x;
+        this.y_destination=y;
+    }
+    public void setCurrent(double x,double y)
+    {
+        this.x_Current=x;
+        this.y_Current=y;
+        picHandler.setX(x);
+        picHandler.setY(y);
+    }
+    public int getX_destination() {
+        return x_destination;
+    }
+    public int getY_destination() {
+        return y_destination;
+    }
+    public double getX_Current() {
+        return x_Current;
+    }
+    public double getY_Current() {
+        return y_Current;
+    }
+    public PicHandler getPicHandler() {
+        return picHandler;
+    }
+
+    public void setSeconds(int seconds) {
+        this.seconds = seconds;
+    }
+
+    public int getSeconds() {
+        return seconds;
+    }
+
 }
